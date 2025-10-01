21a39ac92957a05e7aa526bd82a64bbbdfdacf65
Fredy Arias <fredyariasm@gmail.com>
2025-10-01 19:15:41 -0400

    Connect api

--- src/bookingTimes.js
+++ src/bookingTimes.js
@@ -1,14 +1,19 @@
+import { fetchAPI} from "./api";
+
+
 export const SEED_TIMES = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
 
 export function updateTimes(state, action) {
   switch (action.type) {
     case "dateChanged":
-      return state;
+      const newDate = new Date(action.date);
+      return fetchAPI(newDate);
     default:
       return state;
   }
 }
 
 export function initializeTimes() {
-  return [...SEED_TIMES];
+  const today = new Date();
+  return fetchAPI(today);
 }
